1166f75e03873f569929ab88aa3ae177774ca88a
lamnt1 <lamnt1@intern.firegroup.io>
2025-06-12 00:47:29 +0700

    fix: update baseURL in axios constants to include API version

--- src/apis/axios.constants.ts
+++ src/apis/axios.constants.ts
@@ -1,4 +1,4 @@
-export const baseURL = `${import.meta.env.VITE_API_URL}`;
+export const baseURL = `${import.meta.env.VITE_API_URL}/${import.meta.env.VITE_API_VERSION}`;
 // export const timeout = 10000;
 export const headers = {
   'Content-Type': 'application/json',
