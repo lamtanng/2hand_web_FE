dcfdf28f1e7070e7983a0b0a0de2bad8e3a95b3f
lamnt1 <lamnt1@intern.firegroup.io>
2025-06-12 00:13:45 +0700

    fix: conditionally render recommended products section on HomePage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -38,16 +38,18 @@ const HomePage = () => {
                 <Typography.Title level={2}>Free Products</Typography.Title>
                 <ProductSlider product={freeProduct} isLoading={isLoading} />
               </div>
-              <div>
-                <Typography.Title level={2}>Recommended Products</Typography.Title>
-                <div className="mt-10 grid grid-cols-2 md:grid-cols-3 md:gap-x-0 md:gap-y-6 xl:grid-cols-5">
-                  {recommendedProduct?.map((item: ProductProps) => (
-                    <div key={item._id}>
-                      <ProductCard product={item} isLoading={isLoading} />
-                    </div>
-                  ))}
+              {recommendedProduct?.length > 0 && (
+                <div>
+                  <Typography.Title level={2}>Recommended Products</Typography.Title>
+                  <div className="mt-10 grid grid-cols-2 md:grid-cols-3 md:gap-x-0 md:gap-y-6 xl:grid-cols-5">
+                    {recommendedProduct?.map((item: ProductProps) => (
+                      <div key={item._id}>
+                        <ProductCard product={item} isLoading={isLoading} />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
+              )}
               <Divider />
               <div className="pb-20">
                 <Typography.Title level={2}>About Us</Typography.Title>
